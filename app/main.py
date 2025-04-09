4dd8e4b5db899a8ad9d72c7b672f59f25d9e33a5
Deb228com <130912139+Deb228com@users.noreply.github.com>
2025-04-09 22:45:53 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,10 @@ from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 from app.routers import auth, articles, users, search, guide, about
 from app.database import create_database
-from routes import article  
+from routes import article
+from routes import edit
+
+app.include_router(edit.router)
 
 app.include_router(article.router)
 
